2ff1f4cf0c401e929349b297802ed090406f8c4c
Julia_mb <juliasav2023@gmail.com>
2024-02-09 13:11:23 +0100

    Creación array + Interface
    html+css simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { INoticias } from './interfaces/i-noticias.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Practica_5';
+  arrNoticias: INoticias[] = [
+    {
+      titulo:"primera noticia",
+      imagen: "url.htpjdhn,.com",
+      texto:"hola esta es la primera noticia",
+      fecha:2/2/24,
+    },
+    {
+      titulo:"segunda noticia",
+      imagen: "url.dfsfe,.com",
+      texto:"hola esta es la segunda noticia ",
+      fecha:15/2/24,
+    }    
+  ]
 }
