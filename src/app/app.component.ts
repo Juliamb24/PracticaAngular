d9e6d0f2c99907f4aa8e211872928eb553dfe637
Julia_mb <juliasav2023@gmail.com>
2024-02-11 17:51:14 +0100

    fecha array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,13 @@ export class AppComponent {
       titulo:"Un niño atasca su cabeza en el agujero de una silla",
       imagen: "https://placehold.co/200x200",
       texto:"La adulta que estaba cuidando de este niño no pudo creer lo que estaba viendo. El pequeño jugaba a meter su cabeza por el agujero de la silla cuando, después de meterla por completo, no pudo sacarla.Los dos pasaron unos momentos angustiantes hasta que por fin, igual que entró, salió sin necesidad de los bomberos, asegura Alfonso Arús.",
-      fecha:"21/09/22",
+      fecha:"2021-09-22",
     },
     {
       titulo:"El tatuaje que arrasa en Sevilla (y en el resto de España): un serranito que además genera debate",
       imagen: "https://placehold.co/200x200",
       texto:"En los últimos días, se ha hecho viral el tatuaje, perfectamente ejecutado, que pidió hacerse un sevillano. El dibujo: un auténtico serranito en el que se explica de forma detallada, tanto en diseño como en la información que le acompaña, los ingredientes de este famoso bocata: pan, filete de lomo, pimiento verde, jamón serrano, tomate, alioli y (de nuevo) pan. ",
-      fecha:"04/04/23",
+      fecha:"2020-04-04",
     }    
   ]
 
